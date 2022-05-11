f1a0acdf3974a7b2e024a64852cf57f067dd2a3c
Jordan Padams <jordan.h.padams@jpl.nasa.gov>
2022-05-10 20:52:12 -0700

    Update config to add Github link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,11 +66,27 @@ html_static_path = ['_static']
 html_logo = '_static/images/PDS_Planets.png'
 
 html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-      }
+    'display_github': True,
+    "github_user": "nasa-pds",
+    "github_repo": "nasa-pds.github.io",
+    "github_version": "main/docs/source/"
+}
+
+html_css_files = [
+    'theme_overrides.css',
+]
 
 html_theme_options = {
+    'canonical_url': '',
+    'logo_only': False,
     'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
 }
+
